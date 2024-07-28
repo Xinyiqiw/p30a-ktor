d8a0197bc7517b67c60576f3eb649671f3e3f0f2
Xinyiqi <shinichi_watanabe@dignicate.com>
2024-07-28 23:45:11 +0900

    suppress unused.

--- src/main/kotlin/example/com/Application.kt
+++ src/main/kotlin/example/com/Application.kt
@@ -7,6 +7,7 @@ fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
+@Suppress("unused") // Referenced in application.conf
 fun Application.module() {
     configureRouting()
 }
